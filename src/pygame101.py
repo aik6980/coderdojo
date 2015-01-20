91299473b10eceee895ec26124221d212da6f7d8
aik6980 <aik_canhelp@hotmail.com>
2015-01-20 08:53:53 +0000

    update code

--- src/pygame101.py
+++ src/pygame101.py
@@ -10,10 +10,15 @@ class App:
 
 	def on_execute(self):
 		pygame.init()
-		SCREEN = pygame.display.set_mode(SCREEN_SIZE)
+		SCREEN = pygame.display.set_mode(SCREEN_SIZE) #
 
 		while self.m_active == True:
+			# update input
 			self.handle_input()
+			#update logic/simulation
+
+			#update rendering
+			SCREEN.fill((0, 0, 0))
 			pygame.display.flip()
 
 		pygame.quit()
@@ -22,6 +27,10 @@ class App:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				self.m_active = False
+
+		pressed = pygame.key.get_pressed()
+		if pressed[pygame.K_UP]:
+			print("Key pressed")
 
 if __name__ == "__main__":
 	theApp = App()
